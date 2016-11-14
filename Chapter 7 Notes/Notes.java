8fe835563e93a9713531936ed126e10fab47fe41
Geoff Schmit <gcschmit@gmail.com>
2016-11-14 13:57:08 -0600

    Start of notes on arrays

--- Chapter 7 Notes/Notes.java
+++ Chapter 7 Notes/Notes.java
@@ -7,5 +7,66 @@
  */
 public class Notes
 {
-
+    public static void createArrayOfEvens()
+    {
+        /*
+         * an array stores multiple elements of the same type (e.g., int)
+         * 
+         *  the array is an object that must be declared and initialized
+         *  
+         *  For example, create an array of the first ten even numbers.
+         *      By default, every element has a value of 0
+         */
+        int[] evenNumbers = new int[10];
+        for( int i = 0; i < 10; i++ )
+        {
+            evenNumbers[i] = (i+1) * 2;
+        }
+        
+        /* 
+         * print all the elements in the array
+         *  length returns the number of elements in the array
+         */
+        for( int i = 0; i < evenNumbers.length; i++ )
+        {
+            System.out.println( i + ": " + evenNumbers[i] );
+        }
+    }
+    
+    public static void createArrayOfOdds()
+    {
+        /*
+         * This is an example of an array literal.
+         *  No new operator is used.
+         *  The size is determined based on the number of elements in the
+         *      literal.
+         *  Can only be used when declaring the array.
+         */
+        int[] oddNumbers = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+        
+        // this won't compile
+        //oddNumbers = { 1, 3, 5 };
+    }
+    
+    public static void createArrayOfStrings()
+    {
+        /*
+         * This creates a new array that stores references to five Strings.
+         *  No new strings are created.
+         */
+        String[] words = new String[ 5 ];
+        
+        for( int i = 0; i < words.length; i++ )
+        {
+            words[i] = new String( "" + i );
+        }
+        
+        for( int i = 0; i < words.length; i++ )
+        {
+            System.out.println( i + ": " + words[i] );
+        }
+        
+        
+        
+    }
 }
