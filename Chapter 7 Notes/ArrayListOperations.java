c552c34933a93c50d4b7ea94eb9bde2962183bb2
mlquadri <mlquadri@stu.naperville203.org>
2016-12-07 11:25:23 -0600

    Labs

---
+++ Chapter 7 Notes/ArrayListOperations.java
@@ -0,0 +1,102 @@
+import java.util.ArrayList;
+/**
+ *
+6) Array lists are objects that, like arrays, provide you the ability to store items 
+sequentially and recall items by index. Working with array lists involves invoking ArrayList 
+methods, so we will need to develop some basic skills. Let’s start with the code below:
+
+import java.util.ArrayList;
+
+public class ArrayListRunner
+{
+   public static void main(String[] args)
+   {
+       ArrayList<String> names = new ArrayList<String>();
+       System.out.println(names);
+   }
+} 
+The main method imports java.util.ArrayList and creates an ArrayList that can hold strings. It 
+also prints out the ArrayList and, when it does, we see that the list is empty: [ ].
+
+Complete the following tasks by adding code to this skeleton program. If you are asked to print 
+a value, provide a suitable label to identify it when it is printed.
+
+a) Invoke add() to enter the following names in sequence: Alice, Bob, Connie, David, Edward, 
+Fran, Gomez, Harry. Print the ArrayList again.
+b) Use get() to retrieve and print the first and last names.
+c) Print the size() of the ArrayList.
+d) Use size() to help you print the last name in the list.
+e) Use set() to change “Alice” to “Alice B. Toklas”. Print the ArrayList to verify the change.
+f) Use the alternate form of add() to insert “Doug” after “David”. Print the ArrayList again.
+g) Use an enhanced for loop to print each name in the ArrayList.
+h) Create a second ArrayList called names2 that is built by calling the ArrayList constructor 
+that accepts another ArrayList as an argument. Pass names to the constructor to build names2. 
+Then print the ArrayList.
+i) Call names.remove(0) to remove the first element. Print names and names2. Verify that 
+Alice B. Toklas was removed from names, but not from names2.
+
+ * @author (Me) 
+ * @version (Me)
+ */
+public class ArrayListOperations 
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> names = new ArrayList<String>();
+        System.out.println(names);
+    }
+    
+    public void a(ArrayList<String> names)
+    {
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Connie");
+        names.add("David");
+        names.add("Edward");
+        names.add("Fran");
+        names.add("Gomez");
+        names.add("Harry");
+        System.out.println(names);
+    }
+
+    public void b(ArrayList<String> names)
+    {
+        names.get(0);
+        names.get(names.size());
+    }
+    
+    public void c(ArrayList<String> names)
+    {
+         System.out.println(names.size());
+    }
+    
+    public void d(ArrayList<String> names)
+    {
+        
+    }
+    
+    public void e(ArrayList<String> names)
+    {
+        
+    }
+    
+    public void f(ArrayList<String> names)
+    {
+        
+    }
+    
+    public void g(ArrayList<String> names)
+    {
+        
+    }
+    
+    public void h(ArrayList<String> names)
+    {
+        
+    }
+    
+    public void i(ArrayList<String> names)
+    {
+        
+    }
+}
--- Chapter 7 Notes/ArrayLists.java
+++ Chapter 7 Notes/ArrayLists.java
@@ -11,13 +11,30 @@ public class ArrayLists
     {
         ArrayList<Integer> list=new ArrayList<Integer>();
         fillList(list, 20,10);
+        System.out.println(list);
+        removeEvenElements(list);
+        System.out.println(list);
     }
     public static void fillList(ArrayList<Integer> list, int numValues, int range)
     {
         for(int i=0; i<numValues; i++)
         {
             list.add((int)(Math.random()*range));
-            
+        }
+    }
+    public static void removeEvenElements(ArrayList<Integer> list)
+    {
+        /*
+         * size is like lenght
+         */
+        for(int i = 0; i < list.size(); i++)
+        {
+            int value = list.get(i);
+            if(value%2==0)
+            {
+                list.remove(i);
+                i--;
+            }
         }
     }
 }
